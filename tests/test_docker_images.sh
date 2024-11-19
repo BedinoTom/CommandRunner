a37fc4c6d1758382fe554e3be74a9316a7a938f6
BedinoTom <tom.bedino@etu.univ-cotedazur.fr>
2024-11-19 17:12:42 +0100

    test: add test file

---
+++ tests/test_docker_images.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Chemin local des fichiers de configuration
+CONFIG_DIR="./tests/res"
+
+# Variable de configuration pour le fichier
+ENTRY_CONFIG_FILE="test.json"
+
+# Vérifiez que le répertoire existe
+if [ ! -d "$CONFIG_DIR" ]; then
+  echo "Configuration directory $CONFIG_DIR does not exist."
+  exit 1
+fi
+
+# Lancer les conteneurs avec le répertoire de configuration monté
+docker run --rm -v "$CONFIG_DIR/main:/app" -e CONFIG_FILE="$ENTRY_CONFIG_FILE" kerneltzo/command_runner:main-dev || exit 1
+docker run --rm -v "$CONFIG_DIR/cfssl:/app" -e CONFIG_FILE="$ENTRY_CONFIG_FILE" kerneltzo/command_runner:cfssl-dev || exit 1
+
+echo "All tests passed"
+exit 0
